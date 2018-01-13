78e8bdb265f5d13968e3f8e29a30974a37f27720
Martijn Smit <martijn@lostdomain.org>
2018-01-13 22:32:58 +0100

    - Add the book cover image url and the last annotated date to the book object - Add the page number, note and highlight id to the highlight object

--- lib/kindle_highlights/book.rb
+++ lib/kindle_highlights/book.rb
@@ -1,20 +1,25 @@
 module KindleHighlights
   class Book
-    attr_accessor :asin, :author, :title
+    attr_accessor :asin, :author, :title, :cover_image_url, :last_annotated
 
     def self.from_html_elements(html_element:, mechanize_agent:)
+      m_asin = html_element.attributes["id"].value.squish
       new(
         mechanize_agent: mechanize_agent,
-        asin: html_element.attributes["id"].value.squish,
+        asin: m_asin,
         title: html_element.children.search("h2").first.text.squish,
-        author: html_element.children.search("p").first.text.split(":").last.strip.squish
+        author: html_element.children.search("p").first.text.split(":").last.strip.squish,
+        cover_image_url: html_element.children.search("img.kp-notebook-cover-image").first.attributes["src"].value,
+        last_annotated: html_element.children.search("input#kp-notebook-annotated-date-" + m_asin).first.attributes["value"].value
       )
     end
 
-    def initialize(asin:, author:, title:, mechanize_agent: nil)
+    def initialize(asin:, author:, title:, cover_image_url:, last_annotated:, mechanize_agent: nil)
       @asin = asin
       @author = author
       @title = title
+      @cover_image_url = cover_image_url
+      @last_annotated = last_annotated
       @mechanize_agent = mechanize_agent
     end
 
--- lib/kindle_highlights/highlight.rb
+++ lib/kindle_highlights/highlight.rb
@@ -1,19 +1,35 @@
 module KindleHighlights
   class Highlight
-    attr_accessor :asin, :text, :location
+    attr_accessor :asin, :text, :location, :note, :page, :id
+
+
+    # this function is to determine whether Amazon is giving a location or page number
+    def pagenum(page)
+      if page[0..3] == "Page" 
+        return page.partition(':').last.lstrip
+      else
+        return nil
+      end
+    end
 
     def self.from_html_elements(book:, html_elements:)
       new(
         asin: book.asin,
         text: html_elements.children.search("div.kp-notebook-highlight").first.text.squish,
         location: html_elements.children.search("input#kp-annotation-location").first.attributes["value"].value,
+        note: html_elements.children.search("span#note").first.text,
+        page: html_elements.children.search("span#annotationHighlightHeader").first.text.partition('|').last.lstrip,
+        id: html_elements.children.search("div.kp-notebook-highlight").first.attributes["id"].value.partition('highlight-').last.lstrip
       )
     end
 
-    def initialize(asin:, text:, location:)
+    def initialize(asin:, text:, location:, note:, page:, id:)
       @asin = asin
       @text = text
       @location = location
+      @note = note
+      @page = pagenum(page)
+      @id = id
     end
 
     def to_s
